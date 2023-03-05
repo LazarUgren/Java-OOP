1530a950f12fcb26a33e147f395a1e51d3d74c77
LazarUgren <laza-lazica.94@hotmail.com>
2023-03-05 21:51:26 +0100

    Create ZadatakOsoba1.java

---
+++ Osoba/ZadatakOsoba1.java
@@ -0,0 +1,16 @@
+package Video18Klase.Osoba;
+
+public class ZadatakOsoba1 {
+    String ime;
+    String prezime;
+    int godinaRodjenja;
+    double visina;
+
+    ZadatakOsoba1(String ime1, String prezime1, int godinaRodjenja1, double visina1){
+        ime = ime1;
+        prezime = prezime1;
+        godinaRodjenja = godinaRodjenja1;
+        visina = visina1;
+    }
+
+}
